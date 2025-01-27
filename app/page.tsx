5971312559dd02636f08e342c0ef94db54a69960
KristanLloyd <kristanlloydbalanga@gmail.com>
2025-01-28 00:19:36 +0800

    removed contacts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
-import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 
 export default function Home() {
@@ -13,7 +12,7 @@ export default function Home() {
         <Hero />
         <Projects />
         <Skills />
-        <Contact />
+  
       </main>
       <Footer />
     </div>
